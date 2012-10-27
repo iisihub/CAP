dbc6e21b30ba1d41aaf16bff1813eecc4957c196
Rodes <rodes.chen@gmail.com>
2012-10-27 19:25:23 +0800

    修改css 置換位置 讓 上下頁正常及 直接進入網址正常  ex: 於網址列貼上 page/system/system#codetype  可直接進入 codetype 頁面 並使用正確CSS

--- cap-web/src/main/webapp/static/js/common/cust.common.js
+++ cap-web/src/main/webapp/static/js/common/cust.common.js
@@ -1,42 +1,49 @@
 // init
 $(document).ready(function() {
 	console.log("cust common ready init");
+	var navTop = $("nav.top"), navSub = $("nav.sub");
 	router.set({
 		routes : {
-			"" : "loadpage",      //default route
+			"" : "loadpage", //default route
 			":page" : "loadpage", // http://xxxxx/xxx/#page
 			":page/:page2" : "loadpage" // http://xxxxx/xxx/#page
 		},
 		//router method
 		loadpage : function(page1, page2) {
-			//!page1 && !page2 && ($("#article").html(""));
-			page1 && API.loadPage(page1 + ( page2 ? ('/' + page2) : ''));
-
+			if (page1) {
+				navSub.find('.selected').removeClass('selected');
+				var url = page1 + ( page2 ? ('/' + page2) : '');
+				navSub.find("a[href='" + url + "']").parents("li").children("a").addClass("selected");
+				API.loadPage(url);
+			}
 		}
 	});
-	$("a[href='#language']").click(function(){
+	$("a[href='#language']").click(function() {
 		var o = $(this).parents("ol");
-		if(o.height() == 18) {
+		if (o.height() == 18) {
 			$(this).parent("li.lang").css('background-image', 'url(webroot/static/images/icon-down.png)');
-			$(o).animate({height: 100});
+			$(o).animate({
+				height : 100
+			});
 		} else {
 			$(this).parent("li.lang").css('background-image', 'url(webroot/static/images/icon-right.png)');
-			$(o).animate({height: 18});
+			$(o).animate({
+				height : 18
+			});
 		}
 		return false;
 	});
-	$("nav.top a").click(function(ev){
+	
+	navTop.on("click","a",function(ev) {
 		var _go = $(this).attr("href");
 		var _path = window.location.pathname;
-		var cxtPath = _path.substr(0,_path.indexOf("/",1));
-		window.location.href=cxtPath+"/page/"+_go;
+		var cxtPath = _path.substr(0, _path.indexOf("/", 1));
+		window.location.href = cxtPath + "/page/" + _go;
 		return false;
 	});
-	$("nav.sub a").click(function(ev){
-		var _go = $(this);
-		_go.closest('nav.sub').find('.selected').removeClass('selected');
-		_go.add(_go.parents("li").children("a")).addClass('selected');
-		router.to(_go.attr("href"));
+	
+	navSub.on("click", "a", function(ev) {
+		router.to($(this).attr("href"));
 		ev.preventDefault();
 		return false;
 	});
