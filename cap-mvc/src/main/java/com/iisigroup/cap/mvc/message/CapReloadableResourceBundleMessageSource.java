6a6cbd1be68311a10306df3c897898612352cc2d
Rodes <rodes.chen@gmail.com>
2013-01-23 17:20:30 +0800

    fix weblogic deployment classpath archive to _wl_cls_gen.jar error

--- cap-mvc/src/main/java/com/iisigroup/cap/mvc/message/CapReloadableResourceBundleMessageSource.java
+++ cap-mvc/src/main/java/com/iisigroup/cap/mvc/message/CapReloadableResourceBundleMessageSource.java
@@ -11,11 +11,15 @@
  */
 package com.iisigroup.cap.mvc.message;
 
-import java.io.File;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.io.Resource;
 
 import com.iisigroup.cap.utils.CapString;
 
@@ -29,50 +33,52 @@ import com.iisigroup.cap.utils.CapString;
  * @version <ul>
  *          <li>2011/12/23,rodeschen,new
  *          <li>2011/12/29,rodeschen,fix windows reg
+ *          <li>2013/1/23,roodeschen,fix weglogic deployment error
  *          </ul>
  */
-public class CapReloadableResourceBundleMessageSource extends org.springframework.context.support.ReloadableResourceBundleMessageSource {
+public class CapReloadableResourceBundleMessageSource extends
+		org.springframework.context.support.ReloadableResourceBundleMessageSource implements
+		ApplicationContextAware, InitializingBean {
 
+	protected final Logger logger = LoggerFactory
+			.getLogger(CapReloadableResourceBundleMessageSource.class);
 	private final String i18nFileREG = "(_zh_CN|_en|_zh_TW).properties$";
-	private final String i18nPath = "/i18n";
+	private final String i18nPattern = "classpath:/i18n/**/*.properties";
 
-	public CapReloadableResourceBundleMessageSource() {
-		//Resource[] rs = null;
+	@Override
+	public void setApplicationContext(ApplicationContext ctx) {
+		applicationContext = ctx;
+	}
+
+	private static ApplicationContext applicationContext;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
+	 */
+	@Override
+	public void afterPropertiesSet() throws Exception {
 		Set<String> l = new HashSet<String>();
 		try {
-			List<File> files = getResource(new File(getClass().getResource(i18nPath).getFile()));
-			//rs = AppContext.getApplicationContext().getResources(i18nPath);
-			for (File f : files) {
-				//File f = r.getFile();
-				if (f.isFile()) {
-					String path = f.getAbsolutePath();
-					if (CapString.checkRegularMatch(path, i18nFileREG)) {
-						path = path.replaceAll(i18nFileREG, "").replaceAll(".*/i18n/", "classpath:/i18n/")
-						// for windows
-								.replaceAll(".*\\\\i18n\\\\", "classpath:\\\\i18n\\\\").replaceAll("\\\\", "/");
-						l.add(path);
-					}
+			Resource[] resources = applicationContext.getResources(i18nPattern);
+			for (Resource resource : resources) {
+				String path = resource.getURI().toString();
+				if (CapString.checkRegularMatch(path, i18nFileREG)) {
+					path = path.replaceAll(i18nFileREG, "")
+							.replaceAll(".*/i18n/", "classpath:/i18n/")
+							// for windows
+							.replaceAll(".*\\\\i18n\\\\", "classpath:\\\\i18n\\\\")
+							.replaceAll("\\\\", "/");
+					l.add(path);
 				}
+				logger.debug("set message path:" + path);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		super.setBasenames(l.toArray(new String[l.size()]));
-	}
 
-	private List<File> getResource(File file) {
-		ArrayList<File> fs = new ArrayList<File>();
-		if (file.isDirectory()) {
-			for (File f : file.listFiles()) {
-				if (f.isDirectory()) {
-					fs.addAll(getResource(f));
-				} else {
-					fs.add(f);
-				}
-			}
-		} else {
-			fs.add(file);
-		}
-		return fs;
 	}
 }
