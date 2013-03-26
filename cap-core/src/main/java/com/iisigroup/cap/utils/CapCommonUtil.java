f8d22a3910287e13c95d29c4291c93f0c9a2ef31
iristu <iristu@gmail.com>
2013-03-26 11:14:55 +0800

    spelParser設定 TemplateParserContext為default的ParserContext

--- cap-core/src/main/java/com/iisigroup/cap/utils/CapCommonUtil.java
+++ cap-core/src/main/java/com/iisigroup/cap/utils/CapCommonUtil.java
@@ -15,7 +15,13 @@ import java.math.BigDecimal;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Arrays;
+import java.util.Map;
 
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.ParserContext;
+import org.springframework.expression.common.TemplateParserContext;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.util.Assert;
 
 import com.iisigroup.cap.Constants;
@@ -185,5 +191,41 @@ public class CapCommonUtil {
 		}
 		return hostName;
 	}
+
+	/**
+	 * use Spring Expression Language (SpEL) parse
+	 * 
+	 * @param expressionStr
+	 *            expression string
+	 * @param params
+	 *            parameters
+	 * @param parserContext
+	 *            parserContext
+	 * @return String
+	 */
+	public static String spelParser(String expressionStr,
+			Map<String, Object> params, ParserContext parserContext) {
+		StandardEvaluationContext context = new StandardEvaluationContext(
+				params);
+		ExpressionParser spel = new SpelExpressionParser();
+		return spel.parseExpression(expressionStr, parserContext).getValue(
+				context, String.class);
+	}// ;
+
+	final static ParserContext defParser = new TemplateParserContext();
+
+	/**
+	 * use Spring Expression Language (SpEL) parse
+	 * 
+	 * @param expressionStr
+	 *            expression string
+	 * @param params
+	 *            parameters
+	 * @return String
+	 */
+	public static String spelParser(String expressionStr,
+			Map<String, Object> params) {
+		return spelParser(expressionStr, params, defParser);
+	}// ;
 
 }
