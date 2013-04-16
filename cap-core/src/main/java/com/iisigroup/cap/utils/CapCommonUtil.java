192cdf9d7b76124fe02d53888e9623d1e9275532
iristu <iristu@gmail.com>
2013-04-16 11:03:40 +0800

    add spelParser

--- cap-core/src/main/java/com/iisigroup/cap/utils/CapCommonUtil.java
+++ cap-core/src/main/java/com/iisigroup/cap/utils/CapCommonUtil.java
@@ -15,7 +15,13 @@ import java.math.BigDecimal;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Arrays;
+import java.util.Map;
 
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.ParserContext;
+import org.springframework.expression.common.TemplateParserContext;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.util.Assert;
 
 import com.iisigroup.cap.Constants;
@@ -185,14 +191,15 @@ public class CapCommonUtil {
 		}
 		return hostName;
 	}
-	
+
 	private static String ip;
-	
+
 	/**
 	 * 取得電腦IP
+	 * 
 	 * @return ip address
 	 */
-	public static String getHostIp(){
+	public static String getHostIp() {
 		if (ip == null) {
 			try {
 				ip = InetAddress.getLocalHost().getHostAddress();
@@ -202,5 +209,41 @@ public class CapCommonUtil {
 		}
 		return ip;
 	}
+
+	/**
+	 * use Spring Expression Language (SpEL) parse
+	 * 
+	 * @param expressionStr
+	 *            expression string
+	 * @param params
+	 *            parameters
+	 * @param parserContext
+	 *            parserContext
+	 * @return String
+	 */
+	public static String spelParser(String expressionStr,
+			Map<String, Object> params, ParserContext parserContext) {
+		StandardEvaluationContext context = new StandardEvaluationContext(
+				params);
+		ExpressionParser spel = new SpelExpressionParser();
+		return spel.parseExpression(expressionStr, parserContext).getValue(
+				context, String.class);
+	}// ;
+
+	final static ParserContext defParser = new TemplateParserContext();
+
+	/**
+	 * use Spring Expression Language (SpEL) parse
+	 * 
+	 * @param expressionStr
+	 *            expression string
+	 * @param params
+	 *            parameters
+	 * @return String
+	 */
+	public static String spelParser(String expressionStr,
+			Map<String, Object> params) {
+		return spelParser(expressionStr, params, defParser);
+	}// ;
 
 }
