873ba62172bf372867ad88690d3b66da5234bb2a
tammychen <nunu123@gmail.com>
2014-01-13 16:10:53 +0800

    new 分行維護交易

--- cap-base/src/main/java/com/iisigroup/cap/base/model/Branch.java
+++ cap-base/src/main/java/com/iisigroup/cap/base/model/Branch.java
@@ -19,6 +19,7 @@ import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 
 import com.iisigroup.cap.model.GenericBean;
+import com.iisigroup.cap.model.IDataObject;
 import com.iisigroup.cap.model.listener.CapOidGeneratorListener;
 
 /**
