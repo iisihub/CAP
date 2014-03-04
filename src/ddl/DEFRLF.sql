7bbad146092ae37ce905c9d198c4f9432219ee39
LancelotLiu <lancelot.yl.liu@gmail.com>
2014-03-04 16:26:20 +0800

    remove pgmauth from cc43ccf0540210c3c8e9238c56a3c63fc6cebc3e

--- src/ddl/DEFRLF.sql
+++ src/ddl/DEFRLF.sql
@@ -7,7 +7,7 @@
 create table DEF_RLF (
    ROLCODE              VARCHAR(10)            not null,
    PGMCODE              DECIMAL(6,0)           not null,
-   PGMAUTH              DECIMAL(1,0),
+--   PGMAUTH              DECIMAL(1,0),
 --   PGMDEPT              VARCHAR(80),
 --   AUTOFLAG             CHAR(1),
    UPDATER              VARCHAR(10),
@@ -22,7 +22,7 @@ COMMENT ON DEF_RLF(
 	PGMCODE is '功能代碼',
 --	PGMAUTH is '權限選項',
 --	PGMDEPT is '登入單位代號清單',
-	AUTOFLAG is '0.否 1.是 是否子功能新增時自動在角色內加入該子功能',
+--	AUTOFLAG is '0.否 1.是 是否子功能新增時自動在角色內加入該子功能',
 	UPDATER is '最後異動人員',
 	UPDTIME is '修改日期'
 );
