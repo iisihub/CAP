e274cba8b6a4bc3921bb8d181eecd74f76acd007
tammychen <nunu123@gmail.com>
2014-05-28 17:29:52 +0800

    issue#265

--- cap-base/src/main/java/com/iisigroup/cap/base/handler/ErrorCodeHandler.java
+++ cap-base/src/main/java/com/iisigroup/cap/base/handler/ErrorCodeHandler.java
@@ -54,7 +54,7 @@ import com.iisigroup.cap.utils.CapString;
 @Controller("errorCodehandler")
 public class ErrorCodeHandler extends MFormHandler {
 
-	@Resource(name = "CommonBeanService")
+	@Resource
 	private ICommonService commonSrv;
 
 	@Resource
