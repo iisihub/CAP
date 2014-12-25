a9c06432e26f91e0b7b0f969073c2b255ae5cccb
gverds <gverdsii@gmail.com>
2014-12-25 17:54:31 +0800

    update caseinfo

--- src/ddl/division_initial.sql
+++ src/ddl/division_initial.sql
@@ -19,7 +19,7 @@ create table DIVFTITM (
    TABLENM              VARCHAR(20),
    COLUMNNM             VARCHAR(20),
    DATATYPE             CHAR(1),
-   FACTOREM             VARCHAR(200),
+   FACTORREM            VARCHAR(200),
    FACTJSON             VARCHAR(1000),
    INPUTFLG             CHAR(1)                not null default '1',
    UPDATER              CHAR(6),
@@ -357,250 +357,24 @@ alter table DIVRLDTL
       references DIVRLITM (DIVRLNO)
       on delete restrict;
 
---====================CASE_INFO=================================
+--====================CASEINFO=================================
 --==============================================================
--- Table: CASE_INFO
+-- Table: CASEINFO
 --==============================================================
-create table CASE_INFO (
-   "oid"                CHAR(32),
-   "uid"                CHAR(32),
-   "mainId"             CHAR(32),
-   CPSCASNO             VARCHAR(20)            not null,
-   "---案件資訊---"         CHAR(10),
-   表單種類                 CHAR(1),
-   CPTDDPBN             CHAR(10),
-   docStatus          CHAR(3),
-   "lcdStatus"          CHAR(3),
-   "altStatus"          CHAR(3),
-   "assStatus"          CHAR(3),
-   "accStatus"          CHAR(3),
-   CPTDDBHI             CHAR(4),
-   CIRCIINM             VARCHAR(20),
-   "---分案資訊---"         CHAR(10),
-   DEPARTNO             CHAR(4),
-   GRPUPPID             CHAR(4),
-   CPSGRPID             CHAR(10),
-   PROXYPID             CHAR(10),
-   "---借款人資訊---"        CHAR(10),
-   CPTDDCID             VARCHAR(10),
-   CPTDDCNM             VARCHAR(120),
-   CPTNSCBD             CHAR(10),
-   CPTLNCPY             VARCHAR(120),
-   CPTLNPOS             CHAR(10),
-   CPTLNMAS             CHAR(1),
-   CPTDDCRD             CHAR(1),
-   CPTDDSAM             CHAR(1),
-   擔保                   CHAR(1),
-   保證人                  CHAR(1),
-   展期註記                 CHAR(1),
-   ETABSIND             CHAR(1),
-   CPTNSCOM             CHAR(10),
-   "---註記彙總---"         CHAR(10),
-   CPTDDDCR             CHAR(4),
-   CPTDDREV             CHAR(1),
-   更生結案狀態碼              CHAR(2),
-   CPTDDLIQ             CHAR(1),
-   清算結案狀態碼              CHAR(2),
-   CPTDDDEM             CHAR(1),
-   CPTDDSPC             CHAR(10),
-   CPTDDCRN             CHAR(1),
-   CPTDDSOP1            CHAR(1),
-   CPTDDSOP             CHAR(1),
-   CPTDDSTS             CHAR(1),
-   "---系統資訊---"         CHAR(10),
-   DOCURL               VARCHAR(40),
-   TXCODE               CHAR(6),
-   BATCHDAT             DATE,
-   CPTDDDAT             DATE,
-   CREATOR              CHAR(6),
-   CREATETIME           TIMESTAMP,
-   UPDATER              CHAR(6),
-   UPDATETIME           TIMESTAMP,
-   APPROVER             CHAR(6),
-   APPROVETIME          TIMESTAMP,
-   ISCLOSED             CHAR(1),
-   DELETEDTIME          TIMESTAMP,
-   DATASCHEMA           VARCHAR(5),
-   constraint "P_Key_CASE_INFO_1" primary key (CPSCASNO)
+CREATE TABLE PUBLIC.CASEINFO (
+	OID 		VARCHAR(32) NOT NULL,
+	AMOUNT 		DECIMAL(12,3),
+	CASNO 		VARCHAR(20) NOT NULL,
+	CPSGRPID 	VARCHAR(20),
+	CREATETIME 	TIMESTAMP,
+	CREATOR 	VARCHAR(6),
+	DELETEDTIME TIMESTAMP,
+	DEPARTNO 	VARCHAR(20),
+	DOCSTATUS 	VARCHAR(20),
+	GRPUPPID 	VARCHAR(20),
+	ISCLOSED 	VARCHAR(1),
+	OVERDUEDAY 	INTEGER,
+	UPDATETIME 	TIMESTAMP,
+	UPDATER 	VARCHAR(6),
+	PRIMARY 	KEY (OID)
 );
-
-comment on table CASE_INFO is
-'事後管理追蹤表
-案件資訊 基本資料';
-
-comment on column CASE_INFO."oid" is
-'oid';
-
-comment on column CASE_INFO."uid" is
-'uid';
-
-comment on column CASE_INFO."mainId" is
-'mainId';
-
-comment on column CASE_INFO.CPSCASNO is
-'案件編號';
-
-comment on column CASE_INFO."---案件資訊---" is
-'---案件資訊---';
-
-comment on column CASE_INFO.表單種類 is
-'1.一般 2.KBB';
-
-comment on column CASE_INFO.CPTDDPBN is
-'批覆書編號';
-
-comment on column CASE_INFO."docStatus" is
-'案件狀態';
-
-comment on column CASE_INFO."lcdStatus" is
-'協商狀態';
-
-comment on column CASE_INFO."altStatus" is
-'特殊狀態';
-
-comment on column CASE_INFO."assStatus" is
-'協辦狀態';
-
-comment on column CASE_INFO."accStatus" is
-'帳務狀態';
-
-comment on column CASE_INFO.CPTDDBHI is
-'債權單位';
-
-comment on column CASE_INFO.CIRCIINM is
-'徵信經辦';
-
-comment on column CASE_INFO."---分案資訊---" is
-'---分案資訊---';
-
-comment on column CASE_INFO.DEPARTNO is
-'部門';
-
-comment on column CASE_INFO.GRPUPPID is
-'群組代碼';
-
-comment on column CASE_INFO.CPSGRPID is
-'催收經辦';
-
-comment on column CASE_INFO.PROXYPID is
-'職務代理';
-
-comment on column CASE_INFO."---借款人資訊---" is
-'---借款人資訊---';
-
-comment on column CASE_INFO.CPTDDCID is
-'統一編號';
-
-comment on column CASE_INFO.CPTDDCNM is
-'客戶名稱';
-
-comment on column CASE_INFO.CPTNSCBD is
-'出生年月日';
-
-comment on column CASE_INFO.CPTLNCPY is
-'公司名稱';
-
-comment on column CASE_INFO.CPTLNPOS is
-'職稱';
-
-comment on column CASE_INFO.CPTLNMAS is
-'1.已婚 2.未婚 3.其他';
-
-comment on column CASE_INFO.CPTDDCRD is
-'0.無 1.有效卡 2.流通卡 3.申請停用 4.強制停用 ';
-
-comment on column CASE_INFO.CPTDDSAM is
-'0.否 1.是';
-
-comment on column CASE_INFO.擔保 is
-'0.否 1.是';
-
-comment on column CASE_INFO.保證人 is
-'0.否 1.是';
-
-comment on column CASE_INFO.展期註記 is
-'0.否 1.是';
-
-comment on column CASE_INFO.ETABSIND is
-'0.否 1.是';
-
-comment on column CASE_INFO.CPTNSCOM is
-'特記事項';
-
-comment on column CASE_INFO."---註記彙總---" is
-'---註記彙總---';
-
-comment on column CASE_INFO.CPTDDDCR is
-'債務清理註記';
-
-comment on column CASE_INFO.CPTDDREV is
-'更生註記';
-
-comment on column CASE_INFO.更生結案狀態碼 is
-'更生結案狀態碼';
-
-comment on column CASE_INFO.CPTDDLIQ is
-'清算註記';
-
-comment on column CASE_INFO.清算結案狀態碼 is
-'清算結案狀態碼';
-
-comment on column CASE_INFO.CPTDDDEM is
-'債務展延';
-
-comment on column CASE_INFO.CPTDDSPC is
-'特殊案件註記';
-
-comment on column CASE_INFO.CPTDDCRN is
-'債務協商狀態';
-
-comment on column CASE_INFO.CPTDDSOP1 is
-'個別性一致協商';
-
-comment on column CASE_INFO.CPTDDSOP is
-'停扣註記';
-
-comment on column CASE_INFO.CPTDDSTS is
-'DD狀態';
-
-comment on column CASE_INFO."---系統資訊---" is
-'---系統資訊---';
-
-comment on column CASE_INFO.DOCURL is
-'文件URL';
-
-comment on column CASE_INFO.TXCODE is
-'交易代碼';
-
-comment on column CASE_INFO.BATCHDAT is
-'批次更新使用';
-
-comment on column CASE_INFO.CPTDDDAT is
-'資料更新使用';
-
-comment on column CASE_INFO.CREATOR is
-'建立人員號碼';
-
-comment on column CASE_INFO.CREATETIME is
-'建立日期';
-
-comment on column CASE_INFO.UPDATER is
-'最後異動人員';
-
-comment on column CASE_INFO.UPDATETIME is
-'修改日期';
-
-comment on column CASE_INFO.APPROVER is
-'結案核准人員';
-
-comment on column CASE_INFO.APPROVETIME is
-'核准日期';
-
-comment on column CASE_INFO.ISCLOSED is
-'Y: 已結案 N: 未結案 文件鎖定檢查時使用';
-
-comment on column CASE_INFO.DELETEDTIME is
-'編製中邏輯刪除使用';
-
-comment on column CASE_INFO.DATASCHEMA is
-'來源SCHEMA';
