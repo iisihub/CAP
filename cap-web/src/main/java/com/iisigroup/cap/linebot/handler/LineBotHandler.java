81f02480a8f2ebebd5e347a86313fdf36dff3c14
LancelotLiu <lancelot.yl.liu@gmail.com>
2016-07-20 17:16:06 +0800

    log all request

--- cap-web/src/main/java/com/iisigroup/cap/linebot/handler/LineBotHandler.java
+++ cap-web/src/main/java/com/iisigroup/cap/linebot/handler/LineBotHandler.java
@@ -1,5 +1,7 @@
 package com.iisigroup.cap.linebot.handler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 
 import com.iisigroup.cap.component.Request;
@@ -9,7 +11,12 @@ import com.iisigroup.cap.mvc.handler.MFormHandler;
 
 @Controller("linebothandler")
 public class LineBotHandler extends MFormHandler {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     public Result ask(Request request) {
+        logger.info("========= LINE BOT BEGIN =========");
+        logger.info(request.toString());
+        logger.info("========= LINE BOT END =========");
         AjaxFormResult result = new AjaxFormResult();
         result.set("msg", "ok");
         return result;
