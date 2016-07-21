c7ec5918806d6ef11c71b5d1f960ff31ae3abd62
LancelotLiu <lancelot.yl.liu@gmail.com>
2016-07-21 15:46:33 +0800

    line bot java sdk base import

---
+++ line-ap/src/main/java/com/iisigroup/cap/linebot/web/LineBotRequestFilter.java
@@ -0,0 +1,97 @@
+/* 
+ * LineBotRequestFilter.java
+ * 
+ * Copyright (c) 2016 International Integrated System, Inc. 
+ * All Rights Reserved.
+ * 
+ * Licensed Materials - Property of International Integrated System, Inc.
+ * 
+ * This software is confidential and proprietary information of 
+ * International Integrated System, Inc. (&quot;Confidential Information&quot;).
+ */
+package com.iisigroup.cap.linebot.web;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.linecorp.bot.client.LineBotAPIHeaders;
+import com.linecorp.bot.client.LineBotClient;
+import com.linecorp.bot.client.LineBotClientBuilder;
+import com.linecorp.bot.client.exception.LineBotAPIException;
+
+/**
+ * <pre>
+ * TODO Write a short description on the purpose of the program
+ * </pre>
+ * 
+ * @since 2016年7月21日
+ * @author Lancelot
+ * @version
+ *          <ul>
+ *          <li>2016年7月21日,Lancelot,new
+ *          </ul>
+ */
+public class LineBotRequestFilter implements Filter {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private LineBotClient lineBotClient;
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        String signature = req.getHeader(LineBotAPIHeaders.X_LINE_CHANNEL_SIGNATURE);
+        if (signature != null && signature.length() != 0) {
+            final byte[] json = IOUtils.toByteArray(req.getInputStream());
+            String lineReq = new String(json, StandardCharsets.UTF_8);
+            if (logger.isDebugEnabled()) {
+                logger.debug("got: {}", lineReq);
+            }
+            try {
+                if (!lineBotClient.validateSignature(json, signature)) {
+                    sendError(resp, "Invalid API signature");
+                    return;
+                } else {
+                    request.setAttribute("lineBot", lineReq);
+                    chain.doFilter(request, response);
+                }
+            } catch (LineBotAPIException e) {
+                logger.warn("Invalid API signature", e);
+                sendError(resp, "Internal serve error: " + e.getMessage());
+                return;
+            }
+        } else {
+            // 不是 Line Bot request
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        LineBotClientBuilder lbcb = LineBotClientBuilder.create("1461968250", "8b40651e023e9b296c12196d5675cf45", "uf4d14d3d43b707adf996a6bd6b581118");
+        lineBotClient = lbcb.build();
+    }
+
+    private void sendError(HttpServletResponse resp, String message) throws IOException {
+        logger.warn(message);
+        resp.sendError(HttpServletResponse.SC_BAD_REQUEST, message);
+        resp.setContentType("text/plain; charset=utf-8");
+        resp.getWriter().write(message);
+    }
+}
---
+++ line-ap/src/main/java/com/linecorp/bot/servlet/LineBotCallbackRequestParser.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.linecorp.bot.servlet;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.linecorp.bot.client.LineBotAPIHeaders;
+import com.linecorp.bot.client.LineBotClient;
+import com.linecorp.bot.client.exception.LineBotAPIException;
+import com.linecorp.bot.client.exception.LineBotAPIJsonProcessingException;
+import com.linecorp.bot.model.callback.CallbackRequest;
+
+public class LineBotCallbackRequestParser {
+    private Logger log = LoggerFactory.getLogger(getClass());
+    private final LineBotClient lineBotClient;
+
+    public LineBotCallbackRequestParser(LineBotClient lineBotClient) {
+        this.lineBotClient = lineBotClient;
+    }
+
+    public CallbackRequest handle(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        // validate signature
+        String signature = req.getHeader(LineBotAPIHeaders.X_LINE_CHANNEL_SIGNATURE);
+        if (signature == null || signature.length() == 0) {
+            sendError(resp, "Missing 'X-Line-ChannelSignature' header");
+            return null;
+        }
+
+        final byte[] json = IOUtils.toByteArray(req.getInputStream());
+        if (log.isDebugEnabled()) {
+            log.debug("got: {}", new String(json, StandardCharsets.UTF_8));
+        }
+
+        try {
+            if (!lineBotClient.validateSignature(json, signature)) {
+                sendError(resp, "Invalid API signature");
+                return null;
+            }
+        } catch (LineBotAPIException e) {
+            log.warn("Invalid API signature", e);
+            sendError(resp, "Internal serve error: " + e.getMessage());
+            return null;
+        }
+
+        try {
+            return lineBotClient.readCallbackRequest(json);
+        } catch (LineBotAPIJsonProcessingException e) {
+            sendError(resp, "Invalid Callback");
+            return null;
+        }
+    }
+
+    private void sendError(HttpServletResponse resp, String message) throws IOException {
+        log.warn(message);
+        resp.sendError(HttpServletResponse.SC_BAD_REQUEST,
+                       message);
+        resp.setContentType("text/plain; charset=utf-8");
+        resp.getWriter().write(message);
+    }
+}
